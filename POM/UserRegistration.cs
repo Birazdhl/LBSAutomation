cd9734605a1028a0b6b7bcef9a806ca78bcbb5c5
birazdhl <birazdhl@gmail.com>
2020-07-26 14:04:51 +0545

    Automation Commit

---
+++ POM/UserRegistration.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManagementAutomation.POM
+{
+    class UserRegistration
+    {
+        IWebDriver driver = null;
+        public UserRegistration()
+        {
+            PageFactory.InitElements(StaticDriver.driver, this);
+        }
+
+        [FindsBy(How = How.XPath, Using = "//button[text() = 'Register']")]
+        public IWebElement btnRegister { get; set; }
+
+        public void RegisterUser()
+        {
+            btnRegister.Click();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using LibraryManagementAutomation.POM;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+
+namespace LibraryManagementAutomation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Initialize();
+            p.Test();
+            p.CleanUp();
+        }
+
+        [SetUp]
+        public void Initialize()
+        {
+            StaticDriver.driver = new FirefoxDriver();
+            StaticDriver.driver.Navigate().GoToUrl("http://localhost:3000/");
+        }
+
+        [Test]
+        public void Test()
+        {
+            UserRegistration pageModel = new UserRegistration();
+            pageModel.RegisterUser();
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            StaticDriver.driver.Close();
+            Console.WriteLine("Test Passed");
+        }
+    }
+}
